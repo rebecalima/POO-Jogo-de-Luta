4a3c8fe29f593a1ef1df46a3919ebb1948125948
rebecalima <becalg90@gmail.com>
2018-06-12 02:17:59 +0000

    Nathan Interface Item Item.php

--- trabalho/application/models/Item.php
+++ trabalho/application/models/Item.php
@@ -1,5 +1,17 @@
 <?php
-    interface Item{
-        public function getResistencia();
-    }
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+interface Item {
+    /*
+    * DESCR: O método getItem()
+    * está somente assinado nessa interface, e representa 
+    * o acesso ao nome do Item nas classes que o implementam
+    * AUTOR: Nathan Caraviello Couto
+    * HORAS: 1
+    * ENTRADA: S/ENTRADA
+    * SAÍDA: S/SAÍDA
+    */
+  public function getItem();
+}
+
 ?>
