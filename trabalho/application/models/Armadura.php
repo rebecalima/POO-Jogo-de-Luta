f51b481cab910cd99758fac93758344d0013ac8f
rebecalima <becalg90@gmail.com>
2018-06-12 02:22:05 +0000

    Nathan Construtor Armadura.php

--- trabalho/application/models/Armadura.php
+++ trabalho/application/models/Armadura.php
@@ -1,14 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Armadura implements DefenseItem {
-    function getItem(){
-        
-    }
-    function getResistencia(){
-        
-    }
-    public function getPoderDefesa(){
-        return 3;
+require_once APPPATH."/models/DefenseItem.php";
+
+class Armadura extends DefenseItem {
+    /*
+    * DESCR: O método Armadura é um método construtor da classe Armadura 
+    * que insere os valores respectivos aos atributos herdados da classe abstrata
+    * AUTOR: Nathan Caraviello Couto
+    * HORAS: 1
+    * ENTRADA: Nome do item, Poder de defesa do Armadura
+    * SAÍDA: S/SAÍDA
+    */
+    public function Armadura($nome,$poderDefesa){
+        $this->item = $nome;
+        $this->poderDefesa = $poderDefesa;
     }
 }
 ?>
