a786716f8f58c7f6e032d1be00544867ce00ffba
rebecalima <becalg90@gmail.com>
2018-06-12 02:25:53 +0000

    Nathan registrarDano Jogador.php

--- trabalho/application/models/Jogador.php
+++ trabalho/application/models/Jogador.php
@@ -1,10 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 abstract class Jogador{
-    private $vida, $nome;
+    private $nome;
+    private $vidaTotal;
+    private $vidaAtual;
     private $posicaoAtual = array();
     private $inventario = array();
     
-    public abstract function combater();
-}
-?>
+    /*
+    * DESCR: O método RegistraDano() é um método da classe Jogador 
+    * que registra o dano causado pela ataque da arma do outro jogador,
+    * antes verificando se a vida é menor que o dano, se sim ele chama o método morrer(),
+    * se não, irá perder pontos da vida.
+    * AUTOR: Nathan Caraviello Couto
+    * HORAS: 2
+    * ENTRADA: Dano(ataque da arma), jogador de Defesa, vida do Jogador
+    * SAÍDA: Vida
+    */
+    public function registrarDano($dano, $jogadorDefesa, $vida){
+        if($vida <= $dano){
+            $this->morrer($jogadorDefesa);
+        }else{
+            $vida -= $dano;
+            return $vida;
+        }
+    }
