91847088a27654cec0b345e30dde8f226325cb78
rebecalima <becalg90@gmail.com>
2018-06-12 03:08:23 +0000

    Rebeca configs básicas do codeIgniter

--- trabalho/application/config/config.php
+++ trabalho/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://trabalho-garcia-rebecalima.c9users.io/trabalho/index.php/';
 
 /*
 |--------------------------------------------------------------------------
