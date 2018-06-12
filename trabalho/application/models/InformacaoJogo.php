1fa97006f20ed8ccab8d7d6c375b03f637b24615
rebecalima <becalg90@gmail.com>
2018-06-12 01:31:57 +0000

    Daniel query de equipamentos+ InformacaoJogo.php

--- trabalho/application/models/InformacaoJogo.php
+++ trabalho/application/models/InformacaoJogo.php
@@ -16,5 +16,17 @@ class InformacaoJogo extends CI_Model{
        $cajado = $this->db->query("SELECT * FROM TB_Item WHERE nm_Item = 'Cajado'")->result_array()[0];
        return $cajado;
    }
+      
+   public function armaduraGelo(){
+       $armaduraGelo = $this->db->query("SELECT * FROM TB_Item Where nm_Item = 'Armadura de Gelo'")->result_array()[0];
+       return $armaduraGelo;
+   }
+   public function armaduraFogo(){
+       $armaduraFogo = $this->db->query("SELECT * FROM TB_Item Where nm_Item = 'Armadura de Fogo'")->result_array()[0];
+       return $armaduraFogo;
+   }
+   public function escudo(){
+       $escudo = $this->db->query("SELECT * FROM TB_Item Where nm_Item = 'Escudo'")->result_array()[0];    
+   }
 }
 ?>
