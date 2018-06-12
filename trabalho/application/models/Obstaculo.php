26e6549ec97dd17da13ff24970cdcbaf151c74ff
rebecalima <becalg90@gmail.com>
2018-06-12 01:48:49 +0000

    Rebeca gets Obstaculo.php

--- trabalho/application/models/Obstaculo.php
+++ trabalho/application/models/Obstaculo.php
@@ -8,14 +8,48 @@ class Obstaculo implements Celula{
     /*
     * DESCR: O método Obstaculo() é um método construtor que insere 
     * a posicao do obstaculo no tabuleiro no atributo dessa classe
-    * AUTOR: 
-    * HORAS: --
+    * AUTOR: Rebeca Lima Gomes
+    * HORAS: 1
     * ENTRADA: Posição X, Posição Y
     * SAÍDA: S/SAÍDA
     */
     public function Obstaculo($x,$y){
         $this->idObstaculo["x"] = $x;
         $this->idObstaculo["y"] = $y;
+    }
+    /*
+    * DESCR: O método getImg() retorna a imagem que será usada na tela 
+    * para a célula Obstaculo
+    * AUTOR: Rebeca Lima Gomes
+    * HORAS: 1
+    * ENTRADA: S/ENTRADA
+    * SAÍDA: Caminho da Imagem
+    */
+    public function getImg(){
+        echo "gray";
+    }
+    
+    /*
+    * DESCR: O método existeObstaculo() retorna um booleano indicando 
+    * se é a celula referida é um obstaculo ou não
+    * AUTOR: Rebeca Lima Gomes
+    * HORAS: 1
+    * ENTRADA: S/ENTRADA
+    * SAÍDA: Booleano
+    */
+    public function existeObstaculo(){
+        return true;
+    }
+        
+    /*
+    * DESCR: O método getId() retorna a posição do Obstaculo no tabuleiro
+    * AUTOR: Rebeca Lima Gomes
+    * HORAS: 1
+    * ENTRADA: S/ENTRADA
+    * SAÍDA: Posição do Obstaculo
+    */
+    public function getId(){
+        return $this->idObstaculo;
     }
  
 }
