6948b2eb5aac62c655e1f252568a182b721f9f14
rebecalima <becalg90@gmail.com>
2018-06-12 01:52:50 +0000

    Rebeca makeObstaculo FactoryTabuleiro.php

--- trabalho/application/models/FactoryTabuleiro.php
+++ trabalho/application/models/FactoryTabuleiro.php
@@ -7,7 +7,7 @@ class FactoryTabuleiro{
     * DESCR: O método makeTerreno() cria as instâncias do Terreno de acordo com
     * a quantidade de celulas passada por parametro
     * AUTOR: Rebeca Lima Gomes
-    * HORAS: 1
+    * HORAS: 2
     * ENTRADA: Quantidade de celulas na reta X, Quantidade de celulas na reta Y
     * SAÍDA: S/SAÍDA
     */
@@ -16,6 +16,34 @@ class FactoryTabuleiro{
         for ($x = 0; $x < $limiteX ; $x++){
             for($y = 0; $y < $limiteY; $y++){
                 self::$arrayCelulas[$x][$y] = new Terreno($x,$y);
+            }
+        }
+    }
+    
+        /*
+    * DESCR: O método makeObstaculo() cria as instâncias do Obstaculo de acordo com
+    * a quantidade de obstaculos passada por parametro
+    * AUTOR: Rebeca Lima Gomes
+    * HORAS: 3
+    * ENTRADA: Quantidade de obstaculos
+    * SAÍDA: S/SAÍDA
+    */
+    public static function makeObstaculo($qtdObstaculos){
+        //self::$arrayObstaculos = array();
+        self::$obstaculoX = array();
+        self::$obstaculoY = array();
+        $i = 0;
+        while($i <= $qtdObstaculos){
+            $x = rand(0, $qtdObstaculos);
+            $y = rand(0, $qtdObstaculos);
+            if(self::verificaObstaculo($x, $y, self::$obstaculoX, self::$obstaculoY)){
+                self::$obstaculoX[] = $x;
+                self::$obstaculoY[] = $y;
+                $i++;
+                //var_dump($x);
+                self::$arrayCelulas[$x][$y] = new Obstaculo($x,$y);
+
+                
             }
         }
     }
